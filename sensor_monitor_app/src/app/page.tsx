5c714b41e67284fbe0e655bff544cc851b36a826
Daniel24125 <danielmadalena@msn.com>
2025-01-16 18:40:37 +0000

    starting to implement the design

--- sensor_monitor_app/src/app/page.tsx
+++ sensor_monitor_app/src/app/page.tsx
@@ -5,5 +5,5 @@ import { useUser } from "@auth0/nextjs-auth0"
 export default function Home() {
   const { user, isLoading, error } = useUser()
 
-  return <a href={`/auth/logout`}>Log out</a>
+  return ""
 }
