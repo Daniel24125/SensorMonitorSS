3e897c1b48856f8499bbfce1ecbc7379bb771ab3
Daniel24125 <danielmadalena@msn.com>
2025-01-27 16:07:50 +0000

    fixing sensor form ui

--- sensor_monitor_app/src/app/devices/components/SensorDetails.tsx
+++ sensor_monitor_app/src/app/devices/components/SensorDetails.tsx
@@ -115,104 +115,106 @@ export const SensorForm = ()=>{
         setIsOpen={setOpen}
         title={`${edit ? "Edit the": "Register a"} sensor`}
     >
-        <ScrollArea>
-            <form  action={handleSubmit} className='flex flex-col gap-5 mt-5 h-[75vh]'>
-                <FormInput label='Measurement Mode' description='Select the measurement mode for the pH sensor: acidic (only adjusts the pH for values > 7); alkaline (only adjusts the pH for values < 7); auto (only adjusts the pH for values below and above 7)'>
-                    <Select value={form.mode} onValueChange={(value: PhSensorModeType)=>{
-                        setForm(prev=>{
-                            return {
-                                ...prev, 
-                                "mode": value
-                            }
-                        })
-                    }}>
-                        <SelectTrigger>
-                            <SelectValue placeholder="Measurement Mode" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectItem value="acidic">Acidic Mode</SelectItem>
-                            <SelectItem value="alkaline">Alkaline Mode</SelectItem>
-                            <SelectItem value="auto">Auto Mode</SelectItem>
-                        </SelectContent>
-                    </Select>
-                </FormInput>
-                <FormInput label='Measurement Margin' description='The acceptance margin for the pH sensor, e.g., a margin of 0.1 with a pH target of 7.0 means that pH values of 7.0 ± 0.1 is acceptable'>
-                    <Input
-                        placeholder='Measurement Margin'
-                        id="margin"
-                        name="margin"
-                        value={form.margin}
-                        required
-                        type='number'
-                        step={0.1}
-                        onChange={handleChange}
-                    />
-                </FormInput>
-                <FormInput label='Maximum Valve Openning Time' description='The maximum amount of time the valve will be open'>
-                    <Input
-                        placeholder='Maximum Valve Openning Time'
-                        id="maxValveTimeOpen"
-                        name="maxValveTimeOpen"
-                        value={form.maxValveTimeOpen}
-                        required
-                        type='number'
-                        step={1}
-                        onChange={handleChange}
-                    />
-                </FormInput>
-                <FormInput label='Target pH'>
-                    <Input
-                        placeholder='Target pH'
-                        id="targetPh"
-                        name="targetPh"
-                        value={form.targetPh}
-                        required
-                        type='number'
-                        step={1}
-                        onChange={handleChange}
-                    />
-                </FormInput>
-                <FormInput label='pH Electrode Port'>
-                    <Input
-                        placeholder='pH Electrode Port'
-                        id="probePort"
-                        name="probePort"
-                        value={form.probePort}
-                        required
-                        type='number'
-                        step={1}
-                        onChange={handleChange}
-                    />
-                </FormInput>
-                <FormInput label='Actuation Valve Port' >
-                    <Input
-                        placeholder='Actuation Valve Port'
-                        id="valvePort"
-                        name="valvePort"
-                        value={form.valvePort}
-                        required
-                        type='number'
-                        step={1}
-                        onChange={handleChange}
-                    />
-                </FormInput>
-                <FormInput label='Check Measurment Interval' description='Period of time in which the device will compare the measured pH with the target pH'>
-                    <Input
-                        placeholder='Check Measurment Interval'
-                        id="checkInterval"
-                        name="checkInterval"
-                        value={form.checkInterval}
-                        required
-                        type='number'
-                        step={1}
-                        onChange={handleChange}
-                    />
-                </FormInput>
-                <Button>
-                    Submit
-                </Button>
-            </form>
-        </ScrollArea>
+        <form  action={handleSubmit} className='mt-5 '>
+            <ScrollArea>
+                <div className='flex flex-col gap-5 h-[clamp(300px,75vh,500px)]'>
+                    <FormInput label='Measurement Mode' description='Select the measurement mode for the pH sensor: acidic (only adjusts the pH for values > 7); alkaline (only adjusts the pH for values < 7); auto (only adjusts the pH for values below and above 7)'>
+                        <Select value={form.mode} onValueChange={(value: PhSensorModeType)=>{
+                            setForm(prev=>{
+                                return {
+                                    ...prev, 
+                                    "mode": value
+                                }
+                            })
+                        }}>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Measurement Mode" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="acidic">Acidic Mode</SelectItem>
+                                <SelectItem value="alkaline">Alkaline Mode</SelectItem>
+                                <SelectItem value="auto">Auto Mode</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </FormInput>
+                    <FormInput label='Measurement Margin' description='The acceptance margin for the pH sensor, e.g., a margin of 0.1 with a pH target of 7.0 means that pH values of 7.0 ± 0.1 is acceptable'>
+                        <Input
+                            placeholder='Measurement Margin'
+                            id="margin"
+                            name="margin"
+                            value={form.margin}
+                            required
+                            type='number'
+                            step={0.1}
+                            onChange={handleChange}
+                        />
+                    </FormInput>
+                    <FormInput label='Maximum Valve Openning Time' description='The maximum amount of time the valve will be open'>
+                        <Input
+                            placeholder='Maximum Valve Openning Time'
+                            id="maxValveTimeOpen"
+                            name="maxValveTimeOpen"
+                            value={form.maxValveTimeOpen}
+                            required
+                            type='number'
+                            step={1}
+                            onChange={handleChange}
+                        />
+                    </FormInput>
+                    <FormInput label='Target pH'>
+                        <Input
+                            placeholder='Target pH'
+                            id="targetPh"
+                            name="targetPh"
+                            value={form.targetPh}
+                            required
+                            type='number'
+                            step={1}
+                            onChange={handleChange}
+                        />
+                    </FormInput>
+                    <FormInput label='pH Electrode Port'>
+                        <Input
+                            placeholder='pH Electrode Port'
+                            id="probePort"
+                            name="probePort"
+                            value={form.probePort}
+                            required
+                            type='number'
+                            step={1}
+                            onChange={handleChange}
+                        />
+                    </FormInput>
+                    <FormInput label='Actuation Valve Port' >
+                        <Input
+                            placeholder='Actuation Valve Port'
+                            id="valvePort"
+                            name="valvePort"
+                            value={form.valvePort}
+                            required
+                            type='number'
+                            step={1}
+                            onChange={handleChange}
+                        />
+                    </FormInput>
+                    <FormInput label='Check Measurment Interval' description='Period of time in which the device will compare the measured pH with the target pH'>
+                        <Input
+                            placeholder='Check Measurment Interval'
+                            id="checkInterval"
+                            name="checkInterval"
+                            value={form.checkInterval}
+                            required
+                            type='number'
+                            step={1}
+                            onChange={handleChange}
+                        />
+                    </FormInput>
+                </div>
+            </ScrollArea>
+            <Button className='w-full mt-5'>
+                Submit
+            </Button>
+        </form>
     </ResponsiveDialog>
 }
 
